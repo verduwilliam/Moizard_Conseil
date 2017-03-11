96609ff742b24a0d48870c3d4a770e2e047a627f
William Verdu <verduwilliam@gmail.com>
2017-03-11 21:49:28 +0100

    first commit

---
+++ js/slider.js
@@ -0,0 +1,34 @@
+jssor_1_slider_init = function() {
+
+    var jssor_1_options = {
+      $AutoPlay: true,
+      $SlideDuration: 800,
+      $SlideEasing: $Jease$.$OutQuint,
+      $ArrowNavigatorOptions: {
+        $Class: $JssorArrowNavigator$
+      },
+      $BulletNavigatorOptions: {
+        $Class: $JssorBulletNavigator$
+      }
+    };
+
+    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+    /*responsive code begin*/
+    /*remove responsive code if you don't want the slider scales while window resizing*/
+    function ScaleSlider() {
+        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+        if (refSize) {
+            refSize = Math.min(refSize, 1920);
+            jssor_1_slider.$ScaleWidth(refSize);
+        }
+        else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+    ScaleSlider();
+    $Jssor$.$AddEvent(window, "load", ScaleSlider);
+    $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+    $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+    /*responsive code end*/
+};
